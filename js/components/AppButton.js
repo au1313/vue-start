14d025fcea054169d38ae52113fc1f407049f547
Adam Perkins <adammpkins@gmail.com>
2022-06-17 04:25:05 -0400

    Style and centering using mostly tailwind classes.

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,6 +1,6 @@
 export default {
     template: `
-                <button class="hover:bg-[#43a679]  rounded px-5 py-2 bg-[#4fc08d] text-white font-bold drop-shadow-[0_10px_25px_#435466] disabled:cursor-not-allowed" :disabled="processing">
+                <button class="hover:bg-[#43a679]  w-72 rounded px-5 py-2 bg-[#4fc08d] text-white font-bold drop-shadow-[0_10px_30px_#435466] disabled:cursor-not-allowed m-auto" :disabled="processing">
                     <slot />
                 </button>
     `,
