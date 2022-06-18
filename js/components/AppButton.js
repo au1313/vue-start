aa5d14f9a5ca2260166d95e628f1835dd24fbeab
Adam Perkins <adammpkins@gmail.com>
2022-06-17 21:49:36 -0400

    Refactored App into its own component, App.js

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,6 +1,6 @@
 export default {
     template: `
-                <button class="hover:bg-[#43a679]  w-72 rounded px-5 py-2 bg-[#4fc08d] text-white font-bold drop-shadow-[0_10px_30px_#435466] disabled:cursor-not-allowed m-auto" :disabled="processing">
+                <button class="hover:bg-[#43a679]  w-72 rounded px-5 py-2 bg-[#4fc08d] text-white font-bold mt-10 drop-shadow-[0_10px_50px_#435466] disabled:cursor-not-allowed m-auto" :disabled="processing">
                     <slot />
                 </button>
     `,
