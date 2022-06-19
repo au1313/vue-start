82887188486d9dd2273abb1cab8b9867e6425699
Adam Perkins <adammpkins@gmail.com>
2022-06-19 13:52:52 -0400

    Added launch.json, combined the backup containing the early todo list, and the file containing the app button into the App.js component.

--- js/components/App.js
+++ js/components/App.js
@@ -2,5 +2,49 @@ import AppButton from "./AppButton.js";
 export default {
     components: {
         'app-button': AppButton
-    }
+      },
+    template: `
+          <img class="float-none" src="./img/logo.png" alt="logo" />
+       <!--<div class="flex justify-center button-container">
+         <app-button :processing="true">Submit</app-button>
+       </div> -->
+       <section v-show="inProgressAssignments.length">
+        <h2 class="font-bold mb-2">In Progress</h2>
+        <ul>
+          <li 
+              v-for="assignment in inProgressAssignments"
+              :key="assignment.id"
+          >
+            <label>{{ assignment.name }}
+              <input type="checkbox" v-model="assignment.complete"></li>
+             </label
+             >
+        </ul>
+      </section>
+      <section class="mt-8"  v-show="completedAssignments.length">
+        <h2 class="font-bold mb-2">Completed</h2>
+        <ul>
+          <li v-for="assignment in completedAssignments" :key="assignment.id">
+            <label>{{ assignment.name }}
+              <input type="checkbox" v-model="assignment.complete"></li>
+             </label>
+        </ul>
+      </section>`,
+    data() {
+        return {
+          assignments: [
+            { name: "Finish Project", complete: false, id:1},
+            { name: "Another task", complete: false, id:2 },
+            { name: "Something else", complete: false, id:3 },
+          ],
+        }
+      },
+      computed: {
+        completedAssignments(){
+          return this.assignments.filter(assignment => assignment.complete);
+        },
+        inProgressAssignments(){
+          return this.assignments.filter(assignment => !assignment.complete)
+        }
+      },
 };
