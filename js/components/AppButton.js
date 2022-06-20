c31913d35f557c10f349ebf2c2e66bcaf153cdb7
Adam Perkins <adammpkins@gmail.com>
2022-06-19 22:26:13 -0400

    Styled the checkboxes with tailwind classes and a before element

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -4,7 +4,7 @@ export default {
                     :class="{
                         'w-72 rounded px-5 py-2  text-white font-bold mt-10 drop-shadow-[0_10px_50px_#435466] disabled:cursor-not-allowed m-auto': true,
                         'hover:bg-[#43a679] bg-[#4fc08d]': type === 'primary',
-                        'bg-[#435466] hover:bg-[#3a4959]': type === 'secondary',
+                        'bg-[#35495e] hover:bg-[#293949]': type === 'secondary',
                         'bg-gray-200 hover:bg-gray-400 text-black font-normal': type === 'muted',
                         'is-loading': processing
                     }" 
--- js/components/Assignment.js
+++ js/components/Assignment.js
@@ -1,9 +1,12 @@
 export default {
     template: `
+    <div class="form-check">
         <li>
-            <label>{{ assignment.name }}</label>
-              <input type="checkbox" v-model="assignment.complete">
+            <label class="form-check-label inline-block text-gray-800">{{ assignment.name }}
+                <input class="checked:before:content-['&#10003;'] before:text-white before:relative before:bottom-1 before:left-.5 form-check-input h-4 w-4 border-2 border-gray-300 rounded bg-white checked:bg-[#4fc08d] appearance-none checked:border-[#4fc08d] focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" v-model="assignment.complete">
+            </label>
         </li>
+    </div>
     `,
     props:{
         assignment: Object
