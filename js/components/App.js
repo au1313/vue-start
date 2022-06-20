ae34e20956397a423b3d09f2b011ec8cc7880ea6
Adam Perkins <adammpkins@gmail.com>
2022-06-20 17:46:12 -0400

    Added PWA requirements (icons, manifest, service worker) for eventual PWA integrations. Fixed some responsive styles.

--- js/components/App.js
+++ js/components/App.js
@@ -6,8 +6,10 @@ export default {
         Assignments
       },
     template: `
-
-          <img class="float-none" src="./img/logo.png" alt="logo" />
+          <div class="flex justify-center mt-10">
+            <img class="float-none order-first w-20" src="./img/logo.png" alt="logo" />
+            <h2 class="drop-shadow mt-5 order-last text-xl sm:text-3xl md:text-3xl lg:text-4xl font-bold">Vue<span class="accent">Start</span></h2>
+          </div>
        <!--<div class="flex justify-center button-container">
          <app-button :processing="true">Submit</app-button>
        </div> -->
--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -2,11 +2,11 @@ import AssignmentList from "./AssignmentList.js";
 import AppButton from "./AppButton.js";
 export default {
     template: `
-    <section class="space-y-6">
-        <form @submit.prevent="add">
-            <div class="rounded bg-[#4fc08d] bevel hover:drop-shadow-lg p-3 ">
-                <input v-model="newAssignment" class="mr-1 add-assignment text-black shadow appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline" placeholder="New Assignment" />
-                <app-button type="secondary" >Add</app-button>
+    <section class="space-y-[100px]">
+        <form @submit.prevent="add" class="">
+            <div class=" mt-10 rounded relative left-[-10%]  lg:left-[-60%] md:left-[-30%] sm:left-[-10%] bg-[#4fc08d] lg:w-[600px] sm:w-[300px] w-[300px] md:w-[400px]  bevel hover:drop-shadow-lg p-3 ">
+                <input v-model="newAssignment" class="mr-3 add-assignment lg:w-[490px] md:w-[290px] sm:w-[190px] w-[190px]  text-black shadow appearance-none border rounded py-2 px-3  leading-tight focus:outline-none focus:shadow-outline" placeholder="New Assignment" />
+                <app-button type="secondary">Add</app-button>
             </div>
         </form>
         <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
