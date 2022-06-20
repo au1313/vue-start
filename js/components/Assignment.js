6d0f622a3ef7ad8e80b07119621df5a2fa0677ca
Adam Perkins <adammpkins@gmail.com>
2022-06-19 23:28:10 -0400

    Added styling for assignment component

--- js/components/Assignment.js
+++ js/components/Assignment.js
@@ -1,9 +1,9 @@
 export default {
     template: `
     <div class="form-check">
-        <li>
-            <label class="form-check-label inline-block text-gray-800">{{ assignment.name }}
-                <input class="checked:before:content-['&#10003;'] before:text-white before:relative before:bottom-1 before:left-.5 form-check-input h-4 w-4 border-2 border-gray-300 rounded bg-white checked:bg-[#4fc08d] appearance-none checked:border-[#4fc08d] focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" v-model="assignment.complete">
+        <li class="rounded bg-[#4fc08d] bevel hover:drop-shadow-lg p-3 mt-2">
+            <label class="form-check-label font-bold inline-block text-white">{{ assignment.name }}
+                <input class="checked:before:content-['&#10003;'] before:text-white before:relative before:bottom-1 before:left-.5 form-check-input h-4 w-4 border-2 border-gray-300 rounded bg-white checked:bg-[#35495e] appearance-none checked:border-[#35495e] focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" v-model="assignment.complete">
             </label>
         </li>
     </div>
