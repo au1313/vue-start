0e72a2f8077e05bda7deef4b89a785c3bd9f3a76
Adam Perkins <adammpkins@gmail.com>
2022-06-22 00:23:34 -0400

    made the appbutton processing spinner functional on the assignmentCreate component

--- js/components/AssignmentCreate.js
+++ js/components/AssignmentCreate.js
@@ -6,7 +6,7 @@ export default{
             <form @submit.prevent="add" class="">
             <div class=" mt-10 rounded relative left-[-10%]  lg:left-[-60%] md:left-[-30%] sm:left-[-10%] bg-[#4fc08d] lg:w-[600px] sm:w-[300px] w-[300px] md:w-[400px]  bevel hover:drop-shadow-lg p-3 ">
                 <input v-model="newAssignment" class="mr-3 add-assignment lg:w-[490px] md:w-[290px] sm:w-[190px] w-[190px]  text-black shadow appearance-none border rounded py-2 px-3  leading-tight focus:outline-none focus:shadow-outline" placeholder="New Assignment" />
-                <app-button type="secondary">Add</app-button>
+                <app-button :processing="processing" type="secondary">Add</app-button>
             </div>
         </form>`,
     components: {
@@ -14,13 +14,18 @@ export default{
     },
     data(){
         return{
-            newAssignment: ''
+            newAssignment: '',
+            processing: false,
         }
     },
     methods: {
           add(){
-            this.$emit('add',this.newAssignment);
-            this.newAssignment = ''; 
+            this.processing = true;
+            setTimeout(() =>{
+                this.$emit('add',this.newAssignment);
+                this.newAssignment = ''; 
+                this.processing = false;
+            },200);
         }
     }
 }
--- js/components/AssignmentTags.js
+++ js/components/AssignmentTags.js
@@ -7,7 +7,7 @@ export default{
     template:`
         <div class="flex gap-2 mb-10 ml-[-30px]">
             <app-button @click="$emit('update:currentTag', tag)" :class="{
-                '!bg-[#4fc08d] bevel': tag === modelValue
+                '!bg-[#4fc08d] bevel': tag === currentTag
             }" class="w-30 text-xs" v-for="tag in tags" type="secondary">{{tag}}</app-button>
         </div>
     `,
